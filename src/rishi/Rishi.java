68e6ab98e791f1d15bde64e18dc41a83dcaaf625
S516486 <S516486@NW25097.nwmissouri.edu>
2013-06-04 11:27:43 -0500

    Project Added(Rishi chowdary)

---
+++ src/rishi/Rishi.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rishi;
+
+/**
+ *
+ * @author S516486
+ */
+public class Rishi {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
